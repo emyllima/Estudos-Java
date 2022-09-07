03a9a7e63549f8e9a5ea0fa11ccc67d8d8e25edd
Emylli Lima <emyllit8@gmail.com>
2022-09-06 21:05:32 -0300

    Lista de atividades sobre métodos e funções

---
+++ Lista5/src/ex001.java
@@ -0,0 +1,10 @@
+public class 001 {
+
+    public static void main(String []args) {
+        /*(01) Escreva uma função para receber as dimensões de um retângulo (base e altura) e retornar a sua área.*/
+
+        
+
+    }
+
+}
